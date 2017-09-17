e0a4040526ba1ed755396844805aafa496c9b304
cloudman88 <adamanan88@gmail.com>
2017-09-17 20:30:40 +0300

    commit with Misha

--- server/main.js
+++ server/main.js
@@ -18,16 +18,16 @@ if (req.method === 'POST') {
             requestBody += chunk.toString();
         });
         req.on('end', function() {
-            var data = queryUtil.parse(requestBody);
-            console.log('we have all the data ', data);
+                var data = queryUtil.parse(requestBody);
+                console.log('we have all the data ', data);
                 messages.push(requestBody);
-    while(clients.length > 0) {
-    var client = clients.pop();
-    client.end(JSON.stringify( {
-    count: messages.length,
-    append: requestBody+"\n"}));
-}  
-        })        
+            while(clients.length > 0) {
+                var client = clients.pop();
+                client.end(JSON.stringify( {
+                count: messages.length,
+                append: requestBody+"\n"}));
+            }  
+            })
     }
    
    if(url_parts.pathname == '/') {
@@ -38,68 +38,33 @@ if (req.method === 'POST') {
    } 
    else if(url_parts.pathname.substr(0, 5) == '/poll') {
         // polling code here
-    var count = url_parts.pathname.replace(/[^0-9]*/, '');
-    console.log(count);
-    if(messages.length > count) {
-        res.end(JSON.stringify( {
-        count: messages.length,
-        append: messages.slice(count).join("\n")+"\n"
-        }));
-    } 
-    else {
-        clients.push(res);
-    } 
+        var count = url_parts.pathname.replace(/[^0-9]*/, '');
+        console.log(count);
+        if(messages.length > count) {
+            res.end(JSON.stringify( {
+            count: messages.length,
+            append: messages.slice(count).join("\n")+"\n"
+            }));
+        } 
+        else {
+            clients.push(res);
+        } 
+        res.end("poll end");
    }
    else if(url_parts.pathname.substr(0, 5) == '/msg/') {
-   // message receiving
-   var msg = unescape(url_parts.pathname.substr(5));
-    messages.push(msg);
-    while(clients.length > 0) {
-    var client = clients.pop();
-    client.end(JSON.stringify( {
-    count: messages.length,
-    append: msg+"\n"}));
-  }
-  res.end();
-}}).listen(8080, 'localhost');
+    // message receiving
+        var msg = unescape(url_parts.pathname.substr(5));
+        messages.push(msg);
+        while(clients.length > 0) {
+            var client = clients.pop();
+            client.end(JSON.stringify( {
+            count: messages.length,
+            append: msg+"\n"}));
+          }
+        res.end("msg end");
+   }
+   else {
+       res.end("else end");
+   }
+}).listen(8080, 'localhost');
 console.log('Server running.');
-
-/*
-var http = require('http');
-var urlUtil = require('url');
-var queryUtil = require('querystring');
-
-var server = http.createServer(function(request, response) {
-
-    response.setHeader('Access-Control-Allow-Origin', '*');
-
-    if (request.method === 'GET') {
-                    console.log('inside GET ');
-
-        var url = urlUtil.parse(request.url);
-        var data = queryUtil.parse(url.query);
-        console.log(data.message);
-        if (!data.message) {
-            response.writeHead(400);
-        }
-        response.end();
-    } else if (request.method === 'POST') {
-        console.log('inside POST ');
-        var requestBody = '';
-        request.on('data', function(chunk) {
-            requestBody += chunk.toString();
-        });
-        request.on('end', function() {
-            var data = queryUtil.parse(requestBody);
-            console.log('we have all the data ', data);
-            response.end('thank you');
-        });
-    } else {
-        response.writeHead(405);
-        response.end();
-    }
-});
-
-server.listen(9097);
-console.log('listening...');
-*/
