2a352e95de2f14008778525bc1ace0b4134097cd
cloudman88 <adamanan88@gmail.com>
2017-10-07 14:44:19 +0300

    mid day commit
    editing message list.
    
    editing server function according to requirements

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,7 +4,9 @@ window.Babble = {
     //Babble.postMessage(message:Object, callback:Function)
     postMessage : function postMessage(message, callback){
         console.log('inside postMessage');
+        console.log('sending msg: ', message );
         var request = new XMLHttpRequest();
+
         request.open('POST','http://localhost:9000/messages',true);
         //    var result;
         //    request.onload = function () {
@@ -35,8 +37,9 @@ window.Babble = {
         request.addEventListener("load", function (event) {
                 console.log('added load event listener');
         });
-        request.open('GET', 'http://localhost:9000/poll/'+counter, true);
-
+        var url = 'http://localhost:9000/messages?counter='+counter;
+        //request.open('GET', 'http://localhost:9000/poll/'+counter, true);
+        request.open('GET',url, true);
         request.onload = function() {
             console.log('inside onload');
             if (request.status >= 200 && request.status < 400) {
@@ -44,16 +47,15 @@ window.Babble = {
                 console.log('inside success');
                 var data = JSON.parse(this.responseText);
                 console.log('test: ',data);
-                console.log('counter: ',counter);       
+                console.log('counter: ',counter);
+                // todo addMessageToClient
                 var msgListElement = document.querySelector('.msglist');            
                 for (i = counter; i < data.count; i++) {
                     var li = document.createElement('li');
                     var append = JSON.parse(data.append[i-counter]);
-                    console.log('test00: ',append.message);                
-                    li.appendChild(document.createTextNode(append.message));                    
-                    msgListElement.appendChild(li);
+                    addMessageToClient(append);
                 } 
-               // poll(); 
+                poll(); 
             } else {
                 console.log('inside failure');
                 // We reached our target server, but it returned an error
@@ -68,10 +70,50 @@ window.Babble = {
 };
 
 window.addEventListener('load',function(){
-    var modalElement = document.getElementById('myModal');
-    modalElement.style.display = "block";
+    //if (window.localStorage || localStorage.userInfo.name =="" || localStorage.userInfo.name =="Anonymous")
+    //{    }
+        var modalElement = document.getElementById('myModal');
+        modalElement.style.display = "block";
 },false);
 
+function addMessageToClient(mesgDetails){
+    console.log('mesgDetails: ',mesgDetails);
+    
+    var li = document.createElement('li');
+
+    var cite = document.createElement('cite');
+    cite.innerText = mesgDetails.name+' ';
+    console.log('cite user name : ', cite);
+
+    var d = new Date();
+    d.setTime(mesgDetails.timestamp*1000);
+    console.log('time d: ', d);
+
+    // create timestampFromUnix()
+    var time =document.createElement('time');
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(mesgDetails.timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2);
+    console.log('time formattedTime: ', formattedTime);
+    time.innerText = formattedTime;
+    li.appendChild(cite);
+    li.appendChild(time);
+    var p = document.createElement('p');
+    p.innerText = mesgDetails.message;
+    li.appendChild(p);    
+    var msgListElement = document.querySelector('.msglist');
+    msgListElement.appendChild(li);
+
+
+    //var elem = document.getElementById('data');
+    msgListElement.scrollTop = msgListElement.scrollHeight;
+}
 
 function login(){
     var modalElement = document.getElementById('myModal');
@@ -88,7 +130,8 @@ document.getElementById("msgform").addEventListener('submit', function(event) {
     var eventDetails = JSON.parse(localStorage.getItem('babble'));
     Babble.postMessage({name : eventDetails.userInfo.name,
                 email : eventDetails.userInfo.email,
-                message : document.getElementById('msgBox').value
+                message : document.getElementById('msgBox').value,
+                timestamp : Math.round((new Date()).getTime() / 1000),
     });
     document.getElementById("msgBox").value="";
 },false);
--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ var http = require('http'),
     fs = require('fs'),
     clients = [], //responses
     serverMsgs = [],
-    messages = require('./messages-util.js'),
+    //messages = require('./messages-util.js'),
     id = 0;
 
 //module.exports = serverMsgs;
@@ -22,23 +22,23 @@ http.createServer(function (req, res) {
 
     if (req.method === 'POST') {
         console.log('inside POST');
+        
         if(url_parts.pathname.substr(0, 9) == '/messages') { //new message
             console.log('inside /messages ');
             // message receiving
-            var reqDetails;
-            req.on('data', function(data){
-              //  console.log('req data: ',data.toString());
-                reqDetails = JSON.parse(data);
+            var reqBody;
+            req.on('data', function(data){        
+                reqBody = JSON.parse(data);
             });
-            req.on('end', function(){
-               // console.log("req end");
-                var msg = { name : reqDetails.name, email : reqDetails.email , message : reqDetails.message};
+            req.on('end', function(){ 
+                var msg = { name : reqBody.name, email : reqBody.email ,
+                             message : reqBody.message , timestamp: reqBody.timestamp};
                 // add message
                 //console.log("msgsUtils : ",messages);
-                var id = messages.addMessage(JSON.stringify(msg));   
-                console.log("id : ",id);
-                //messages.push(JSON.stringify(msg));
-                console.log("msg : ",msg);
+                //var id = messages.addMessage(JSON.stringify(msg));   
+                serverMsgs.push(JSON.stringify(msg));
+                var counter =serverMsgs.length;
+                console.log("id : ",counter,"msg : ",msg);
                 console.log("serverMsgs: ",serverMsgs);
                 while(clients.length > 0) {
                     var client = clients.pop();
@@ -46,35 +46,43 @@ http.createServer(function (req, res) {
                     count: serverMsgs.length,
                     append: [JSON.stringify(msg)] }));
                 }
-                res.end(JSON.stringify({id:42}));
+                res.end(JSON.stringify({id:counter})); //return count of msgs
             });
         }
     }   
     else if(req.method == 'GET'){
         console.log('inside GET');
+        console.log('url_parts.path',url_parts.path.substr(0, 18));
+
         if(url_parts.pathname == '/') {
             // file serving
             fs.readFile('../client/index.html', function(err, data) {
                 res.end(data);
             });
         } 
-        else if(url_parts.pathname.substr(0, 5) == '/poll') { //change to /messages?counter=XX 
-                // polling code here
-                var count = url_parts.pathname.replace(/[^0-9]*/, '');
-                console.log('inside poll, count: ',count,'serverMsgs.length: ', serverMsgs.length);
-                if(serverMsgs.length > count) {
-                    console.log('inside serverMsgs.length > count');                
+        
+        else if(url_parts.path.substr(0, 18) == '/messages?counter=') { // polling
+                // polling code here                                
+                var captured = /counter=([^&]+)/.exec(url_parts.path)[1]; // Value is in [1] ('384' in our case)
+                var counter = captured ? captured : 0;                
+                console.log('inside poll. ', 'serverMsgs.length: ', serverMsgs.length);
+                console.log('captured:',captured,'counter:',counter);               
+                if(serverMsgs.length > counter) {
+                    console.log('inside serverMsgs.length > counter');                
                     res.end(JSON.stringify({
                         count: serverMsgs.length,
-                        append: serverMsgs.slice(count)}));
+                        append: serverMsgs.slice(counter,serverMsgs.length)}));
                 } 
                 else {
                     console.log('client push ');
                     clients.push(res);
                 }
-        }else if(url_parts.pathname.substr(0, 6) == '/stats') { //get statistics
+        }
+        
+        else if(url_parts.pathname.substr(0, 6) == '/stats') { //get statistics
                 // todo
         }
+        
         else {
             res.end("else end");
         }
