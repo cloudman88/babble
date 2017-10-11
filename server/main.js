562b2e47c6ab081b7da273d33f75cce0d98b9404
cloudman88 <adamanan88@gmail.com>
2017-10-11 23:04:04 +0300

    server handle errors

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ var http = require('http'),
     messages = require('./messages-util.js');
 
 http.createServer(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -18,9 +18,15 @@ http.createServer(function (req, res) {
     var url_parts = url.parse(req.url);
     console.log('inside req: ',req.method);
 
-    if (req.method === 'POST') {
-        
-        if(url_parts.pathname.substr(0, 9) == '/messages') { //new message        
+    if (req.method === 'POST') {        
+        if ((url_parts.pathname.includes("messages?counter")) || //GET
+            (url_parts.pathname.includes("stats")) || //GET
+            (url_parts.pathname.includes("messages?")) || //GET
+            (url_parts.pathname.includes("/messages/"))) { //DELETE
+            methodNotAllowedError(res,url_parts.pathname,req.method);
+        }
+
+        else if(url_parts.pathname.substr(0, 9) == '/messages') { //new message        
             console.log('inside /messages ');
             // message receiving
             var reqBody;
@@ -32,10 +38,7 @@ http.createServer(function (req, res) {
                             message : reqBody.message , timestamp: reqBody.timestamp,
                             emailHash: md5(reqBody.email)};
                 
-                var counter = messages.addMessage(msg);
-                console.log("id : ",counter,"msg : ",msg);
-                console.log("new msg clients.length : ",clients.length);
-                
+                var counter = messages.addMessage(msg);            
                 while(clients.length > 0) {
                     var client = clients.pop();
                     client.end(JSON.stringify( {
@@ -45,12 +48,8 @@ http.createServer(function (req, res) {
                 console.log('new msg statsResponses.length ',statsResponses.length);
                 console.log('new msg responseBody in login ',{users:usersCounter , messages:messages.getMsgCounter()});
                 
-                try {
-                    while(statsResponses.length > 0) {
-                        console.log('new msg looop');
-                        var statsRes = statsResponses.pop();
-                        statsRes.end(JSON.stringify( {users:usersCounter , messages:messages.getMsgCounter()}));
-                    }
+                try {                    
+                    sendStatsResponses();
                 }
                 catch(e){
                     console.log("error caught:",e);
@@ -71,12 +70,8 @@ http.createServer(function (req, res) {
                 console.log('usersCounter after ++ ',usersCounter);
                 console.log('statsResponses.length ',statsResponses.length);
                 console.log('responseBody in login ',{users:usersCounter , messages:messages.getMsgCounter()});
-                while(statsResponses.length > 0) {
-                    console.log('looop');
-                    var statsRes = statsResponses.pop();
-                    statsRes.end(JSON.stringify( {users:usersCounter , messages:messages.getMsgCounter()}));
-                }
-                res.end(JSON.stringify(true));
+                sendStatsResponses();                
+                res.end();
             });
         }
 
@@ -91,43 +86,55 @@ http.createServer(function (req, res) {
                 console.log('usersCounter after -- ',usersCounter);    
                 console.log('statsResponses.length  ',statsResponses.length);    
                 console.log('responseBody in logout ',{users:usersCounter , messages:messages.getMsgCounter()});
-                while(statsResponses.length > 0) {
-                    var statsRes = statsResponses.pop();
-                    statsRes.end(JSON.stringify( {users:usersCounter , messages:messages.getMsgCounter()}));
-                }
+                sendStatsResponses();
                 res.end();
             });
         }
         else {
-            console.log('inside else!!!');
-            console.log(req.url);
-            res.writeHead(404); // 404 non-existent URLs (not found)
-            res.end(); // 404 non-existent URLs (not found)
+            nonExistentURLError(res,url_parts.pathname);
         }
 
     }   
     else if(req.method == 'GET'){        
+        if ((url_parts.pathname.includes("login")) || //POST
+            (url_parts.pathname.includes("logout")) || //POST
+            (url_parts.pathname.includes("/messages/")) || //DELETE
+            (url_parts.pathname.includes("/messages" && !(url_parts.pathname.includes("messages?counter"))))  //POST
+            ) {
+                methodNotAllowedError(res,url_parts.pathname,req.method); 
+        }
 
-        if(url_parts.pathname == '/') {
+        else if(url_parts.pathname == '/') {
             // file serving
             fs.readFile('../client/index.html', function(err, data) {
                 res.end(data);
             });
         }
         
-        else if(url_parts.path.substr(0, 18) == '/messages?counter=') { // polling
-                var captured = /counter=([^&]+)/.exec(url_parts.path)[1];
-                var counter = captured ? captured : 0;     
-                var msgCounter = messages.getMsgCounter();
-                if(msgCounter > counter) {
-                    console.log('inside messages.serverMsgs.length > counter');                
-                    res.end(JSON.stringify({
-                        count: msgCounter,
-                        append: messages.getMessages(counter)}));
+        else if(url_parts.path.substr(0, 10) == '/messages?') { // polling
+                if (url_parts.path.substr(0, 18) == '/messages?counter='){
+                    var captured = /counter=([^&]+)/.exec(url_parts.path)[1];               
+                    var counter = captured ? captured : 0;
+                    console.log("captured: ",captured,"counter: ",counter);
+                    if (isFinite(String(counter))==true){
+                        var msgCounter = messages.getMsgCounter();
+                        if(msgCounter > counter) {
+                            console.log('inside messages.serverMsgs.length > counter');                
+                            res.end(JSON.stringify({
+                                count: msgCounter,
+                                append: messages.getMessages(counter)}));
+                        }
+                        else {
+                            console.log('client push ');
+                            clients.push(res);
+                        }
+                    }
+                    else{
+                        badRequestError(res,url_parts.path,req.method);
+                    } 
                 }
-                else {
-                    console.log('client push ');
-                    clients.push(res);
+                else{
+                        badRequestError(res,url_parts.path,req.method);
                 }
         }
         
@@ -137,39 +144,75 @@ http.createServer(function (req, res) {
         }
         
         else {
-            res.writeHead(404); // 404 non-existent URLs (not found)
-            res.end();
+            nonExistentURLError(res,url_parts.pathname);
         }
     }
 
-    else if (req.method == 'OPTIONS'){
+    else if (req.method == 'OPTIONS'){                
         res.writeHead(204); //OPTIONS request (sometimes sent automatically by the browser)
         res.end();
     }
 
     else if(req.method == 'DELETE'){
-        if(url_parts.path.substr(0, 10) == '/messages/') {
+         if ((url_parts.pathname.includes("login")) || //POST
+            (url_parts.pathname.includes("logout")) || //POST
+            (url_parts.pathname.includes("messages?")) || //GET
+            (url_parts.pathname.includes("messages?counter")) || //GET
+            (url_parts.pathname.includes("stats")) || //GET
+            (url_parts.pathname.includes("/messages" && !(url_parts.pathname.includes("/messages/"))))  //POST
+            ) {
+               methodNotAllowedError(res,url_parts.pathname,req.method);
+        }
+
+        else if(url_parts.path.substr(0, 10) == '/messages/') {
                 var id = url_parts.path.substr(10, url_parts.path.length); 
-                console.log('id in delete: ',id);               
-                var countBeforeDelete = messages.getMsgCounter();
-                messages.deleteMessage(id);
-                var countAfterDelete = messages.getMsgCounter();
-                if (countBeforeDelete > countAfterDelete){
-                    while(statsResponses.length > 0) {
-                        var statsRes = statsResponses.pop();
-                        statsRes.end(JSON.stringify( {users:usersCounter , messages:messages.getMsgCounter() }));
+                 if (isFinite(String(id))==true){
+                    console.log('id in delete: ',id);               
+                    var countBeforeDelete = messages.getMsgCounter();
+                    messages.deleteMessage(id);
+                    var countAfterDelete = messages.getMsgCounter();
+                    if (countBeforeDelete > countAfterDelete){
+                        sendStatsResponses();
+                        res.end(JSON.stringify(true));          
                     }
-                    res.end(JSON.stringify(true));          
-                }
-                else{
-                    res.end(JSON.stringify(false));
-                }
+                    else{
+                        res.end(JSON.stringify(false));
+                    }
+                 }
+                 else {
+                     badRequestError(res,url_parts.path,req.method);
+                 }
         }
         else{
-           res.writeHead(404); // 404 non-existent URLs (not found)
-            res.end();
+          nonExistentURLError(res,url_parts.pathname);
         }
     //todo
     }
     }).listen(9000, 'localhost');
 console.log('Server running on port: '+ 9000);
+
+function sendStatsResponses(){
+        while(statsResponses.length > 0) {
+            console.log('new msg looop');
+            var statsRes = statsResponses.pop();
+            statsRes.end(JSON.stringify( {users:usersCounter , messages:messages.getMsgCounter()}));
+        }
+}
+
+function methodNotAllowedError(res,path,reqMethod){
+    console.log("method: ",path," is not allowed in request method of type ",reqMethod);
+    res.writeHead(405);  //405 = method not allowed
+    res.end(); 
+}
+
+function nonExistentURLError(res,path){
+    console.log("URL: ",path,"  not found ");
+    res.writeHead(404); // 404 = non-existent URLs (not found)
+    res.end();
+}
+
+function badRequestError(res,path,reqMethod){
+    console.log("Invalid data sent in URL: ",path," method typed ",reqMethod);
+    res.writeHead(400); // 400 = bad request
+    res.end();
+}
